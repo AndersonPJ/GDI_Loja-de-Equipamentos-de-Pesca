30304597b9963312ffb008779905c5f7e910dfef
AndersonPJ <andersonhenrique.ahbs@gmail.com>
2019-10-23 17:44:58 -0300

    Finalizando arquivo de conexao com a DataBase

--- src/Controller/ConnectionDataBase.java
+++ src/Controller/ConnectionDataBase.java
@@ -1,9 +1,35 @@
 package Controller;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
 /**
  *
  * @author AndersonPJ
  */
 public class ConnectionDataBase {
-    
+	public static Connection getConnection () {
+        Connection myConnection = null;
+        
+        // Configuracao do servidor
+        String server = "oracle12c.cin.ufpe.br";
+        String port = "1521";
+        String database = "Instance01";
+        String url = "jdbc:oracle:thin:@" + server + ":" + port + ":" + database;
+        
+        // Dados do usuario
+        String username = "";
+        String password = "";
+        
+        try {
+        	Class.forName("oracle.jdbc.driver.OracleDriver");
+        	myConnection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+        	e.printStackTrace();
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+        
+        return myConnection;
+	}
 }
