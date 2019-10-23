16446e20c18f9ea75a5fd478f7c299edacc17158
AndersonPJ <andersonhenrique.ahbs@gmail.com>
2019-10-23 17:24:14 -0300

    Finalizando Model de FUNCIONARIO

--- src/Model/Funcionario.java
+++ src/Model/Funcionario.java
@@ -5,5 +5,45 @@ package Model;
  * @author AndersonPJ
  */
 public class Funcionario {
+    private int id_funcionario, salario;
+    private String cargo, ctps;
     
+    public Funcionario (int id_funcionario, int salario, String cargo, String ctps) {
+        this.setId_funcionario(id_funcionario);
+        this.setSalario(salario);
+        this.setCargo(cargo);
+        this.setCtps(ctps);
+    }
+
+    public int getId_funcionario() {
+        return id_funcionario;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setId_funcionario(int id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
 }
