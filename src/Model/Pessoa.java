960f45f277798bb35a9f27b6467515cc613386be
AndersonPJ <andersonhenrique.ahbs@gmail.com>
2019-10-23 16:00:49 -0300

    Finalizando Model de Pessoa

--- src/Model/Pessoa.java
+++ src/Model/Pessoa.java
@@ -1,9 +1,60 @@
 package Model;
+import java.sql.Date;
 
 /**
  *
  * @author AndersonPJ
  */
 public class Pessoa {
+    private int id_pessoa;
+    private String cpf, nome, sexo;
+    private Date data_nascimento;
+
+    public Pessoa (int id_pessoa, String cpf, String nome, String sexo, Date data_nascimento) {
+        this.id_pessoa = id_pessoa;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.data_nascimento = data_nascimento;
+    }
     
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
 }
