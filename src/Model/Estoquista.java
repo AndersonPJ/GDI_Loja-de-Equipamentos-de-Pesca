35b35efea05a3546dd073dbaf1ab7d0084921685
AndersonPJ <andersonhenrique.ahbs@gmail.com>
2019-10-23 17:24:35 -0300

    Finalizando Model de ESTOQUISTA

--- src/Model/Estoquista.java
+++ src/Model/Estoquista.java
@@ -5,5 +5,17 @@ package Model;
  * @author AndersonPJ
  */
 public class Estoquista {
+    private int id_estoquista;
     
+    public Estoquista (int id_estoquista) {
+        this.setId_estoquista(id_estoquista);
+    }
+
+    public int getId_estoquista() {
+        return id_estoquista;
+    }
+
+    public void setId_estoquista(int id_estoquista) {
+        this.id_estoquista = id_estoquista;
+    }
 }
